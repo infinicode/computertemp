684913da048d6fd6710b41e221d99ca9208efb8c
fito <infinicode@users.noreply.github.com>
2008-10-28 18:22:45 +0000

    Updated license headers

--- computertemp/computertemp/temp_adt746x.py
+++ computertemp/computertemp/temp_adt746x.py
@@ -1,32 +1,32 @@
 # -*- coding: UTF-8 -*-
 
-# +-------------------------------------------------------------------------------------+
-# | GPL											|
-# +-------------------------------------------------------------------------------------+
-# | Copyright (c) 2005,2006 Adolfo González Blázquez <code@infinicode.org>	|
-# |                         Francesco Accattapà <callipeo@libero.it>			|
-# |											|
-# | This program is free software; you can redistribute it and/or			|
-# | modify it under the terms of the GNU General Public License				|
-# | as published by the Free Software Foundation; either version 2			|
-# | of the License, or (at your option) any later version.				|
-# |											|
-# | This program is distributed in the hope that it will be useful,			|
-# | but WITHOUT ANY WARRANTY; without even the implied warranty of			|
-# | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			|
-# | GNU General Public License for more details.					|
-# |											|
-# | You should have received a copy of the GNU General Public License			|
-# | along with this program; if not, write to the Free Software				|
-# | Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.	|
-# +-------------------------------------------------------------------------------------+
+"""
+Copyright (C) 2005-2008 Adolfo González Blázquez <code@infinicode.org>
+						Francesco Accattapà <callipeo@libero.it>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+If you find any bugs or have any suggestions email: code@infinicode.org
+"""
 
 import os.path
 
 class TempFuncs:
 
 	def __init__(self):
-		
+
 		self.thermal_path = '/sys/devices/temperatures/'
 		self.thermal_suffix = '_temperature'
 		self.all_sensors = ('sensor1', 'sensor2')
@@ -49,7 +49,7 @@ class TempFuncs:
 	def get_zones(self):
 		if self.temp_support():
 			return list(self.all_sensors)
-	
+
 	# Return zone name at position num
 	def get_zone_name(self, num):
 		if self.temp_support():
@@ -60,8 +60,8 @@ class TempFuncs:
 	def get_zone_display_name(self, num):
 		if self.temp_support():
 			return self.sensors_display_name[num]
-		
-	
+
+
 	# Reads temperature from zone
 	def get_zone_temp(self, zone):
 		if self.temp_support():
@@ -74,4 +74,4 @@ class TempFuncs:
 				return None
 			else:
 				return 0
-			
+
